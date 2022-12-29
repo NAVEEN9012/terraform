5ebe1d637380d6918e7f201d6ffa82b5c77810d2
NAVEEN9012 <71424664+NAVEEN9012@users.noreply.github.com>
2022-12-29 15:29:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "728a4094-1648-4ee9-b7f6-2c509c6b5a68"
-  client_id       = "fd3809b9-07d4-4fff-9a72-bb2f3c966958"
-  client_secret   = "rdS8Q~.7nwFlyhjL07urS3BzaF9_UEYPVOJBxc4K"
-  tenant_id       = "36591f76-8672-4875-9ddb-9de77b3204db"
-}
 
 
 resource "azurerm_resource_group" "example" {
